18a5c15c9e359f1a92c78172327551940ac18cce
zzr-ie <1722735969@qq.com>
2025-10-28 10:54:14 +0800

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -256,9 +256,7 @@ def cost_page_layout():
                 options=[
                     {"label": "Total Inventory", "value": "Total Inventory"},
                     {"label": "Pads", "value": "Pads"},
-                    {"label": "Disc", "value": "Disc"},
-                    {"label": "Moto", "value": "Moto"},
-                    {"label": "Fluid", "value": "Fluid"}
+                    {"label": "Disc", "value": "Disc"}
                 ],
                 value="Total Inventory"
             )
@@ -1925,8 +1923,7 @@ import math
     Output("dio-inventory-graph", "figure"),
     Input("inv-start-date", "value"),
     Input("inv-end-date", "value"),
-    Input("inv-category", "value"),
-    Input("category-selector", "value")
+    Input("inv-category", "value")
 )
 def update_dio_inventory_chart(start_date, end_date, category):
     if inventory_df.empty or df_store.empty:
@@ -1938,10 +1935,6 @@ def update_dio_inventory_chart(start_date, end_date, category):
             return df[s.str.startswith("2000")]
         elif category == "Disc":
             return df[s.str.startswith(("2330","2300"))]
-        elif category == "Moto":
-            return df[s.str.startswith("34N00037")]
-        elif category == "Fluid":
-            return df[s.str.startswith("34N00039")]
         elif category == "Total Inventory":
             return df
         return df
